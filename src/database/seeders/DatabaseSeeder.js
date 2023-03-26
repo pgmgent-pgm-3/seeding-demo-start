a135b47bdfb5eb159b065e9d30e67d90acb0156c
Frederick Roegiers <frederick@rogerthat.be>
2023-03-26 16:17:39 +0200

    wip

--- src/database/seeders/DatabaseSeeder.js
+++ src/database/seeders/DatabaseSeeder.js
@@ -3,15 +3,16 @@ import { DataSource } from "typeorm";
 import entities from "../../models/index.js";
 
 // connect to database
-class DatabaseSeeder {
+export default class DatabaseSeeder {
   constructor(type, database, entities) {
     this.type = type;
     this.database = database;
     this.entities = entities;
+    this.connection = null;
   }
 
   async connect() {
-    const connection = new DataSource({
+    this.connection = new DataSource({
       type: this.type,
       database: this.database,
       entities: this.entities,
